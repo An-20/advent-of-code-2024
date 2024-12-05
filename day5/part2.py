81fed233a4e9a2bbb536b13ad97ebe3e56ecb5a3
An-20 <71339471+An-20@users.noreply.github.com>
2024-12-05 06:52:32 +0000

    added day 5

---
+++ day5/part2.py
@@ -0,0 +1,32 @@
+with open("input.txt") as file:
+    sections = [x.strip() for x in file.read().split("\n\n") if x.strip()]
+
+
+rules = {}
+for row in sections[0].splitlines():
+    a, b = row.split("|")
+    a, b = int(a), int(b)
+    if b in rules:
+        rules[b].append(a)
+    else:
+        rules[b] = [a]
+
+
+s = 0
+for row in sections[1].splitlines():
+    data = [int(x.strip()) for x in row.split(",") if x.strip()]
+    works = True
+    # n squared, who cares?
+    for i in range(len(data)):
+        for j in range(i + 1, len(data)):
+            a, b = data[i], data[j]
+            if a in rules and b in rules[a]:
+                data[i], data[j] = data[j], data[i]
+                works = False
+    if works:
+        continue
+    print(data)
+    s += data[len(data) // 2]
+
+
+print(s)
