c0a5bbbcb7bebbe41a5c7f84dd6fc3c09d09c30c
An-20 <71339471+An-20@users.noreply.github.com>
2024-12-06 07:24:33 +0000

    improved silk solution (reduced copies)

--- day6/part2_multi.py
+++ day6/part2_multi.py
@@ -1,4 +1,5 @@
-import itertools
+import time
+
 from utils.silk import execute_parallel
 
 
@@ -6,7 +7,7 @@ with open("input.txt") as file:
     rows = [x.strip() for x in file.read().splitlines() if x.strip()]
 
 
-def loops(obx, oby, rows=None):
+def fn(oby, rows=None):
     DELTAS = {
         0: (0, -1),
         1: (1, 0),
@@ -14,45 +15,52 @@ def loops(obx, oby, rows=None):
         3: (-1, 0)
     }
 
-    rot = 0
-    x = None
-    y = None
-    for ridx, row in enumerate(rows):
-        for cidx, char in enumerate(row):
-            if char == "^":
-                x = cidx
-                y = ridx
+    s = 0
 
-    positions = {(x, y)}
+    for obx in range(len(rows[0])):
+        rot = 0
+        x = None
+        y = None
+        for ridx, row in enumerate(rows):
+            for cidx, char in enumerate(row):
+                if char == "^":
+                    x = cidx
+                    y = ridx
 
-    while True:
-        dx, dy = DELTAS[rot]
-        px = x + dx
-        py = y + dy
-        if not 0 <= py < len(rows) or not 0 <= px < len(rows[0]):
-            break
-        if rows[py][px] == "#" or (px == obx and py == oby):
-            rot = (rot + 1) % 4
-            continue
-        x = px
-        y = py
-        if (x, y, rot) in positions:
-            return 1
-        positions.add((x, y, rot))
-    return 0
+        positions = {(x, y)}
+
+        while True:
+            dx, dy = DELTAS[rot]
+            px = x + dx
+            py = y + dy
+            if not 0 <= py < len(rows) or not 0 <= px < len(rows[0]):
+                break
+            if rows[py][px] == "#" or (px == obx and py == oby):
+                rot = (rot + 1) % 4
+                continue
+            x = px
+            y = py
+            if (x, y, rot) in positions:
+                s += 1
+                break
+            positions.add((x, y, rot))
+
+    return s
 
 
 ROWS = len(rows)
 COLS = len(rows[0])
-TOTAL = ROWS * COLS
 
 
 def main():
     out = execute_parallel(
-        [loops] * TOTAL, list(itertools.product(range(COLS), range(ROWS))), [{"rows": rows}] * TOTAL
+        [fn] * ROWS, [[x] for x in range(ROWS)], [{"rows": rows}] * ROWS
     )
     print(sum(x.obj for x in out))
 
 
 if __name__ == "__main__":
+    start_time = time.perf_counter()
     main()
+    end_time = time.perf_counter()
+    print(end_time - start_time)
