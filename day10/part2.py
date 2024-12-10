271e2829269eb08a4ffaeb565028d5e03f350850
An-20 <71339471+An-20@users.noreply.github.com>
2024-12-10 06:41:08 +0000

    added day 10

---
+++ day10/part2.py
@@ -0,0 +1,41 @@
+import time
+import functools
+
+with open("input.txt") as file:
+    rows = [x.strip() for x in file.read().splitlines() if x.strip()]
+
+
+ROWS = len(rows)
+COLS = len(rows[0])
+
+
+@functools.lru_cache()
+def count(x: int, y: int, depth: int = 0):
+    if rows[y][x] == "9" and depth == 9:
+        return 1
+    if int(rows[y][x]) != depth:
+        return 0
+    s = 0
+    for step in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+        dx, dy = step
+        nx = x + dx
+        ny = y + dy
+        if not (0 <= nx < COLS and 0 <= ny < ROWS):
+            continue
+        s += count(nx, ny, depth + 1)
+    return s
+
+
+def main():
+    s = 0
+    for row in range(ROWS):
+        for col in range(COLS):
+            s += count(col, row, 0)
+    print(s)
+
+
+if __name__ == "__main__":
+    start_time = time.perf_counter()
+    main()
+    end_time = time.perf_counter()
+    print(end_time - start_time)
