7f79e30931f3c1da9a2d962a27a349f5f99aefba
An-20 <71339471+An-20@users.noreply.github.com>
2024-12-16 23:15:59 +0000

    use heapq instead of O(n^2) searches

--- day16/part1.py
+++ day16/part1.py
@@ -1,4 +1,4 @@
-from collections import deque
+import heapq
 
 with open("input.txt") as file:
     maze = [x.strip() for x in file.read().split("\n") if x.strip()]
@@ -14,51 +14,31 @@ end_y = [x for x in range(len(maze)) if "E" in maze[x]][0]
 
 DIRS = [(1, 0), (0, 1), (-1, 0), (0, -1)]
 
-visited = {}
 distances = {}
+queue = [(0, start_x, start_y, 0)]
 
 for x in range(len(maze[0])):
     for y in range(len(maze)):
         if maze[y][x] == "#":
             continue
         for d in range(4):
-            visited[(x, y, d)] = False
             distances[(x, y, d)] = 1_000_000_000
 
-distances[(start_x, start_y, 0)] = 0
 
-
-for idx in range(len(distances)):
-    if idx % 1000 == 0:
-        print(f"{idx}/{len(distances)}")
-
-    lowest_val = None
-    lowest_dist = 1_000_000_000_000
-
-    # O(n^2)
-    # but hey its faster to spend 5 minutes waiting than to rewrite this?
-    for is_visited, node, distance in zip(visited.values(), distances.keys(), distances.values()):
-        if not is_visited and distance < lowest_dist:
-            lowest_dist = distance
-            lowest_val = node
-
-    if not lowest_val:
-        raise ValueError("unreachable!")
-
-    x, y, d = lowest_val
-    dist = lowest_dist
+while queue:
+    dist, x, y, d = heapq.heappop(queue)
     dx, dy = DIRS[d]
     if maze[y + dy][x + dx] != "#":
         if dist + 1 < distances[(x + dx, y + dy, d)]:
             distances[(x + dx, y + dy, d)] = dist + 1
+            heapq.heappush(queue, (dist + 1, x + dx, y + dy, d))
 
     for nd in range(4):
         if d == nd:
             continue
         if dist + 1000 < distances[(x, y, nd)]:
             distances[(x, y, nd)] = dist + 1000
-
-    visited[(x, y, d)] = True
+            heapq.heappush(queue, (dist + 1000, x, y, nd))
 
 
 print(min(distances[(end_x, end_y, d)] for d in range(4)))
--- day16/part2.py
+++ day16/part2.py
@@ -1,4 +1,4 @@
-from collections import deque
+import heapq
 
 with open("input.txt") as file:
     maze = [x.strip() for x in file.read().split("\n") if x.strip()]
@@ -14,65 +14,47 @@ end_y = [x for x in range(len(maze)) if "E" in maze[x]][0]
 
 DIRS = [(1, 0), (0, 1), (-1, 0), (0, -1)]
 
-visited = {}
 distances = {}
 prevs = {}
+queue = [(0, start_x, start_y, 0)]
 
 for x in range(len(maze[0])):
     for y in range(len(maze)):
         if maze[y][x] == "#":
             continue
         for d in range(4):
-            visited[(x, y, d)] = False
             distances[(x, y, d)] = 1_000_000_000
-            prevs[(x, y, d)] = []
 
 
-distances[(start_x, start_y, 0)] = 0
-
-
-for idx in range(len(distances)):
-    if idx % 1000 == 0:
-        print(f"{idx}/{len(distances)}")
-
-    lowest_val = None
-    lowest_dist = 1_000_000_000_000
-
-    # O(n^2)
-    # but hey its faster to spend 5 minutes waiting than to rewrite this?
-    for is_visited, node, distance in zip(visited.values(), distances.keys(), distances.values()):
-        if not is_visited and distance < lowest_dist:
-            lowest_dist = distance
-            lowest_val = node
-
-    if not lowest_val:
-        raise ValueError("unreachable!")
-
-    x, y, d = lowest_val
-    dist = lowest_dist
+while queue:
+    dist, x, y, d = heapq.heappop(queue)
     dx, dy = DIRS[d]
+
     if maze[y + dy][x + dx] != "#":
         if dist + 1 < distances[(x + dx, y + dy, d)]:
             distances[(x + dx, y + dy, d)] = dist + 1
-            prevs[(x + dx, y + dy, d)] = [lowest_val]
+            prevs[(x + dx, y + dy, d)] = [(x, y, d)]
+            heapq.heappush(queue, (dist + 1, x + dx, y + dy, d))
+
         elif dist + 1 == distances[(x + dx, y + dy, d)]:
-            prevs[(x + dx, y + dy, d)].append(lowest_val)
+            prevs[(x + dx, y + dy, d)].append((x, y, d))
 
     for nd in range(4):
         if d == nd:
             continue
         if dist + 1000 < distances[(x, y, nd)]:
             distances[(x, y, nd)] = dist + 1000
-            prevs[(x, y, nd)] = [lowest_val]
-        elif dist + 1000 == distances[(x, y, nd)]:
-            prevs[(x, y, nd)].append(lowest_val)
+            prevs[(x, y, nd)] = [(x, y, d)]
+            heapq.heappush(queue, (dist + 1000, x, y, nd))
 
-    visited[(x, y, d)] = True
+        elif dist + 1000 == distances[(x, y, nd)]:
+            prevs[(x, y, nd)].append((x, y, d))
 
 
 best = min(distances[(end_x, end_y, d)] for d in range(4))
 
 to_explore = []
+visited = set()
 for d in range(4):
     if distances[(end_x, end_y, d)] == best:
         to_explore.append((end_x, end_y, d))
@@ -80,6 +62,9 @@ for d in range(4):
 tiles = set()
 while to_explore:
     node = to_explore.pop()
+    if node in visited:
+        continue
+    visited.add(node)
     x, y, d = node
     tiles.add((x, y))
     to_explore.extend(prevs[node])
